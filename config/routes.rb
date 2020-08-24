6cedce8640b893d6a09f1a9732fccd27bde69f19
simeo russo <simeo94@hotmail.com>
2020-08-24 17:41:28 +0100

    added routes and conrollers

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/dashboard" ,to: "pages#dashboard"
+
+ resources :user_subscriptions, only: [ :show, :new, :create, :edit, :update, :destroy] do
+    resources :reminders, only: [:new, :create, :edit,:update]
+  end
+  resources :reminders, only: [:destroy]
 end
