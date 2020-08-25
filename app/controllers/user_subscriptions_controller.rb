5a46fdf060f805ec6979ea4834c8f8494beccb8e
Shreya Gupta <shreyagupta2906@gmail.com>
2020-08-25 12:42:10 +0100

    added a 'new' user journey part to it, there is a new subscription user page and added the relevant methods to the controller

--- app/controllers/user_subscriptions_controller.rb
+++ app/controllers/user_subscriptions_controller.rb
@@ -1,23 +1,34 @@
 class UserSubscriptionsController < ApplicationController
+  def new
+    @user_subscription = UserSubscription.new
+  end
 
-# def new
-# end
+  def create
+    @user_subscription = UserSubscription.new(user_subscription_params)
+    @user_subscription.save
+    redirect_to user_subscription_path(@user_subscription)
+  end
 
-# def create
-# end
+  # def edit
+  #   @user_subscription = User_Subscription.find(params[:id])
+  # end
 
-# def edit
-# end
+  # def update
+  #   @user_subscription = User_Subscription.find(params[:id])
+  #   @user_subscription.update(user_subscription_params)
+  #   redirect_to user_subscription_path(@user_subscription)
+  # end
 
-# def update
-# end
+  # def destroy
+  #   @user_subscription = User_Subscription.find(params[:id])
+  #   @user_subscription.destroy
+  #   redirect_to user_subscription_path
+  # end
 
-# def destroy
-# end
+  private
 
-# private
-
-# def user_subscription_params
-# end
-
+  def user_subscription_params
+    params.require(:user_subscription).permit(:start_date, :end_date, :payment_date, :cost,
+    :type, :user_id, :subscription_id)
+  end
 end
