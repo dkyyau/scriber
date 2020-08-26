45100d40527946a09434a18e173492b1a7c6a749
Dorothy Yau <dkyyau@gmail.com>
2020-08-26 17:41:14 +0100

    added billing plan table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_26_153532) do
+ActiveRecord::Schema.define(version: 2020_08_26_163825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "billing_plans", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "categories", force: :cascade do |t|
