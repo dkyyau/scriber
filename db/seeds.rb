ac4141c9c5592aa2b06ebaf1f6d2e5afec98ce44
Dorothy Yau <dkyyau@gmail.com>
2020-08-27 14:40:35 +0100

    Started seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,48 +6,38 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+BillingPlan.destroy_all
+Category.destroy_all
+Subscription.destroy_all
+
+puts "Creating billing plans..."
+
+BillingPlan.create(name: "Free trial")
+BillingPlan.create(name: "Weekly")
+BillingPlan.create(name: "Monthly")
+BillingPlan.create(name: "3 months")
+BillingPlan.create(name: "12 months")
+BillingPlan.create(name: "24 months")
+BillingPlan.create(name: "Lifetime")
+
  puts "Creating categories..."
 
-Category.create(name: "TV")
-Category.create(name: "Film")
-Category.create(name: "Music")
-Category.create(name: "Phone bills")
-Category.create(name: "Gym membership")
+Category.create(name: "Entertainment")
+Category.create(name: "Cloud storage")
+Category.create(name: "Software")
+Category.create(name: "Food and drink")
+Category.create(name: "Beauty")
+Category.create(name: "Phone")
+Category.create(name: "Health")
+Category.create(name: "Utilities")
 
 puts "Creating subscriptions..."
 
-Subscription.create!(
-  name: "Netflix",
-  description: "A movies/tv show streaming service",
-  price: 14.99,
-  category: Category.first
-  )
-
-Subscription.create(
-  name: "Spotify",
-  description: "A music streaming service",
-  price: 10.99,
-  category: Category.first
-  )
-
-Subscription.create(
-  name: "Playstation Now",
-  description: "A videogames streaming service",
-  price: 7.99,
-  category: Category.first
-  )
-
-Subscription.create(
-  name: "Crunchyroll",
-  description: "A anime streaming service",
-  price: 4.99,
-  category: Category.first
-  )
-
-puts "Creating User Subscriptions..."
-
-UserSubscription.create(cost: 4.99, subscription: Subscription.first, user: User.first)
-
-
+# Subscription.create(
+#   name: "Netflix - Basic",
+#   description: "Unlimited films, TV programmes and more. Watch anywhere. Cancel at any time.",
+#   price: 5.99,
+#   category_id: 
+# )
 
 puts "Finished!"
