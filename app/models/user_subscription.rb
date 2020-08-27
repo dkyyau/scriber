241dfde79631ab76a0e879fd9aeb06762d0859f5
Shreya Gupta <shreyagupta2906@gmail.com>
2020-08-27 11:46:33 +0100

    changed period to plan

--- app/models/user_subscription.rb
+++ app/models/user_subscription.rb
@@ -3,5 +3,4 @@ class UserSubscription < ApplicationRecord
   belongs_to :subscription
   validates :payment_date, presence: true
   validates :cost, presence: true
-  validates :billing_period, presence: true
 end
