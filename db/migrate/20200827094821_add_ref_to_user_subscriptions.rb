03328e39841d2d6e286964e55607258414b11ea7
Dorothy Yau <dkyyau@gmail.com>
2020-08-27 10:49:47 +0100

    added billing plans table

--- db/migrate/20200827094821_add_ref_to_user_subscriptions.rb
+++ db/migrate/20200827094821_add_ref_to_user_subscriptions.rb
@@ -1,4 +1,4 @@
-class AddRefsToUserSubscriptions < ActiveRecord::Migration[6.0]
+class AddRefToUserSubscriptions < ActiveRecord::Migration[6.0]
   def change
     add_reference :user_subscriptions, :billing_plan, null: false, foreign_key: true
   end
