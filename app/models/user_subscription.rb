7c3308b712472def785016f280fc551a90667d8d
Shreya Gupta <shreyagupta2906@gmail.com>
2020-09-01 15:02:11 +0100

    nested form but need to fix it

--- app/models/user_subscription.rb
+++ app/models/user_subscription.rb
@@ -4,6 +4,7 @@ class UserSubscription < ApplicationRecord
   belongs_to :user
   belongs_to :subscription
   has_many :reminders, dependent: :destroy
+  accepts_nested_attributes_for :reminders
   belongs_to :billing_plan
   validates :payment_date, presence: true
   validates :cost, presence: true
