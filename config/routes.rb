c0b7ade7e1089b4c06200b19d21437bed619c822
Dorothy Yau <dkyyau@gmail.com>
2020-09-03 10:26:47 +0100

    Various CSS fixes

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   post "/admin", to: "pages#send_reminder", as: :send_reminders
 
   resources :user_subscriptions, only: [ :show, :new, :create, :edit, :update, :destroy] do
-    resources :reminders, only: [ :index, :show, :new, :create, :edit,:update]
+    resources :reminders, only: [ :index, :show, :new, :create, :edit, :update ]
   end
   resources :reminders, only: [:destroy]
 end
